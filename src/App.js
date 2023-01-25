28f2a60d50e4ed561aab16f41b0fdb8aded12349
Chris Nguyen <chrishiepnguyen@yahoo.com>
2023-01-25 16:33:19 -0600

    created login and register pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import Home from "./pages/Home";
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
 // import ProductList from "./pages/ProductList";
 
 function App() {
   return (
-    <Product />
+    <Login />
   );
 }
 
