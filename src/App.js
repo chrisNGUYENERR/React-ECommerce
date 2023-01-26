74b8d6cbea760dcf0b57c379d51790305db7022c
Chris Nguyen <chrishiepnguyen@yahoo.com>
2023-01-25 21:20:22 -0600

    created cart page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import Home from "./pages/Home";
 // import Product from "./pages/Product";
-import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
 // import Register from "./pages/Register";
 // import ProductList from "./pages/ProductList";
 
 function App() {
   return (
-    <Login />
+    <Cart />
   );
 }
 
